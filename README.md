51f965bcee4f6629cabdf40c086818d70384ebb4
James Strong <strong.james.e@gmail.com>
2017-06-02 17:46:16 -0400

    added the config file options

--- README.md
+++ README.md
@@ -1,2 +1,64 @@
 # ghosts
-Manage multiple STS creds for shared AWS accounts
+Ghosts - Go implementation that will help manage multiple STS credentials for shared AWS accounts
+
+-profile will write the the temporary credentails to the defaul ~/.aws/credentials profile name in the option
+
+
+Leaving -profile blank will make GHOSTS print the export varables so that they can be set as environment variables
+
+### Ghost CLI options
+ghosts --help
+
+Usage of ghosts:
+
+  -arn string Role ARN
+
+  -base string base profile assuming (default "default")
+
+  -config string  Config file that contains assume role informations
+
+  -debug debug output
+
+  -duration int number of seconds credentials will last (default 900)
+
+  -mfa indicates if a mfa is need for this role
+
+  -name string name of the session (default "sts-sesssion")
+
+  -profile string profile to write credentials out too
+
+  -serial string MFA serial number, arn:aws:iam::123456789012:mfa/user
+
+  -token string MFA token value
+
+    	
+    	
+ ### Using a Config file
+ 
+ GHOSTS will parse the file provided in the --config option and set the flags for 
+ 
+  -arn 
+     	
+  -base 
+   
+  -serial 
+      	
+  -profile
+ 
+ Config File Syntax
+ 
+ [profile1]
+ base="base1"
+ role="arn:aws:iam::[ACCOUNT_NUMBER]:role/[ROLENAME]"
+ profile="[PROFILE_TO_UPDATE]"
+ mfa_serial="arn:aws:iam::[ACCOUNT_NUMBER]:mfa/[IAM_USERNAME]"
+ 
+ [profile2]
+ base="base1"
+ role="arn:aws:iam::[ACCOUNT_NUMBER]:role/[ROLENAME]"
+ profile="[PROFILE_TO_UPDATE]"
+ mfa_serial="arn:aws:iam::[ACCOUNT_NUMBER]:mfa/[IAM_USERNAME]"
+ 
+ 
+ https://docs.aws.amazon.com/sdk-for-go/api/service/sts/#pkg-overview
+ 
--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"github.com/go-ini/ini"
 	"os"
 	"path/filepath"
+	"github.com/aws/aws-sdk-go/aws/awserr"
 )
 
 func getIniLocation() string {
@@ -49,7 +50,53 @@ func writeIni(sectionName string, credentials *sts.AssumeRoleOutput) {
 	cfg.Section(sectionName).NewKey("aws_session_token", aws.StringValue(credentials.Credentials.SessionToken))
 	cfg.SaveTo(iniLocation)
 
+
 }
+
+func parseConfig(section string, config string){
+
+	cfg, err := ini.Load(config)
+	if err != nil{
+		fmt.Print("Error loading config: %v", err)
+		os.Exit(3)
+	}
+
+	sec, err := cfg.GetSection(section)
+	if err != nil{
+		fmt.Print("Section %v not found in config: %v", section, err)
+		os.Exit(3)
+	}
+
+	base,err := sec.GetKey("base")
+	if err != nil{
+		fmt.Print("Base profile required in config: %v", err)
+		os.Exit(3)
+	}
+
+	role, err := sec.GetKey("role")
+	if err != nil{
+		fmt.Print("Role required in config: %v", err)
+		os.Exit(3)
+	}
+
+	mfa_serial,err  := sec.GetKey("mfa_serial")
+	if err != nil{
+		fmt.Print("MFA Serial required in config: %v", err)
+		os.Exit(3)
+	}
+
+	profile, err := sec.GetKey("profile")
+	if err != nil{
+		fmt.Print("Profile required in config: %v", err)
+		os.Exit(3)
+	}
+
+	flag.Set("base", base.String())
+	flag.Set("arn", role.String())
+	flag.Set("serial", mfa_serial.String())
+	flag.Set("profile", profile.String())
+}
+
 
 //Profile Name
 
@@ -66,8 +113,19 @@ func main() {
 	mfa_bool := flag.Bool("mfa", false, "indicates if a mfa is need for this role")
 	mfa_token := flag.String("token", "","MFA token value")
 	mfa_serial := flag.String("serial","","MFA serial number, arn:aws:iam::123456789012:mfa/user")
+	debug := flag.Bool("debug", false, "debug output")
+
+	config := flag.String("config", "", "Config file that contains assume role informations")
 
 	flag.Parse()
+
+	if *mfa_token != "" {
+		*mfa_bool = true
+	}
+
+	if *config != ""{
+		parseConfig(*profile, *config)
+	}
 
 
 	sess := session.Must(session.NewSessionWithOptions(session.Options{
@@ -106,26 +164,38 @@ func main() {
 		}
 	}
 
+	if *debug {
+		fmt.Printf("DEBUG: Params %v", params)
+	}
+
 	resp, err := svc.AssumeRole(params)
 
 	if err != nil {
 		// Print the error, cast err to awserr.Error to get the Code and
 		// Message from an error.
 		fmt.Println(err.Error())
-		return
+
+		fmt.Println(err.(awserr.Error))
+
+		os.Exit(2)
 	}
 
 	if *profile == "" {
 		os.Setenv("AWS_ACCESS_KEY_ID", aws.StringValue(resp.Credentials.AccessKeyId))
 		os.Setenv("AWS_SECRET_ACCESS_KEY", aws.StringValue(resp.Credentials.SecretAccessKey))
 		os.Setenv("AWS_SESSION_TOKEN", aws.StringValue(resp.Credentials.SessionToken))
-
+		fmt.Printf("export AWS_ACCESS_KEY_ID=%s\n", os.Getenv("AWS_ACCESS_KEY_ID"))
+		fmt.Printf("export AWS_SECRET_ACCESS_KEY=%s\n", os.Getenv("AWS_SECRET_ACCESS_KEY"))
+		fmt.Printf("export AWS_SESSION_TOKEN=%s\n", os.Getenv("AWS_SESSION_TOKEN"))
+		fmt.Printf("export AWS_SECURITY_TOKEN=\"$AWS_SESSION_TOKEN\"\n")
 
 	}else {
 		writeIni(*profile, resp)
 	}
 
 	// Pretty-print the response data.
-	//fmt.Println(resp)
+	if *debug {
+		fmt.Println(resp)
+	}
 
 }
